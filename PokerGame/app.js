4095034aa8d544ad27c966b655d2543afae0e212
Wamique Abro <wamik.abro212@gmail.com>
2023-10-09 16:49:45 +0500

    poker game basic structure using javascript

---
+++ PokerGame/app.js
@@ -0,0 +1,12 @@
+let startingPokerChips = 100; // points
+let players = 3;
+let noOfStarterCards = 2;
+
+let playerOnePoints = startingPokerChips;
+let playerTwoPoints = startingPokerChips;
+let playerThreePoints = startingPokerChips;
+
+playerOnePoints -= 50;
+playerTwoPoints -=25;
+playerThreePoints +=75;
+
