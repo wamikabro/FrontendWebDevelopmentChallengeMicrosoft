69e4025eb17349fedc38ef2db5c85e7167aa9809
Wamique Abro <wamik.abro212@gmail.com>
2023-10-09 17:01:14 +0500

    add an intro to the game

--- PokerGame/app.js
+++ PokerGame/app.js
@@ -10,6 +10,9 @@ let playerOnePoints = STARTING_POKER_CHIPS;
 let playerTwoPoints = PLAYERS;
 let playerThreePoints = NO_OF_STARTER_CARDS;
 
+console.log(`Welcome to Texas Hold'em. The championship title will be awarded to one of these three players: ${playerOneName}, ${playerTwoName} and ${playerThreeName}. Each player has ${STARTING_POKER_CHIPS} in their pot. We have an exciting game ahead of us. May the best player win!`)
+
+
 playerOnePoints -= 50;
 playerTwoPoints -=25;
 playerThreePoints +=75;
