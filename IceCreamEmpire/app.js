81338c236ad977f482740ac16a7499600d673086
Wamique Abro <wamik.abro212@gmail.com>
2023-10-09 19:20:17 +0500

    add functionality to calculate the sale of each flavor

--- IceCreamEmpire/app.js
+++ IceCreamEmpire/app.js
@@ -18,3 +18,12 @@ transactions.push({ scoops: ["Vanilla", "Vanilla"], total: 4 })
 const total = transactions.reduce((acc, curr) => acc + curr.total, 0);
 console.log(`You've made ${total} $ today`); // You've made 11.5 $ today
 
+let flavorDistribution = transactions.reduce((acc, curr) => {
+    curr.scoops.forEach(scoop => {
+      if (!acc[scoop]) {
+        acc[scoop] = 0;
+      }
+      acc[scoop]++;
+    })
+    return acc;
+  }, {}) // { Chocolate: 1, Vanilla: 3, Mint Chip: 1, Raspberry: 1, StrawBerry: 1 }
