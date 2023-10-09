38ce3ce426897492bd65c2e9483feeecce9fdf56
Wamique Abro <wamik.abro212@gmail.com>
2023-10-09 19:18:24 +0500

    add customer interactions using array

--- IceCreamEmpire/app.js
+++ IceCreamEmpire/app.js
@@ -7,3 +7,11 @@ let iceCreamFlavors = [
     { name: "Mint Chip", type: "Chocolate", price: 1.5 },
     { name: "Raspberry", type: "Fruit", price: 1},
    ];
+
+// { scoops: [], total: }
+let transactions = []
+
+transactions.push({ scoops: ["Chocolate", "Vanilla", "Mint Chip"], total: 5.5 })
+transactions.push({ scoops: ["Raspberry", "StrawBerry"], total: 2 })
+transactions.push({ scoops: ["Vanilla", "Vanilla"], total: 4 })
+
